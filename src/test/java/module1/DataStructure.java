03fd7acad04fb020ca2436f241475a0a2f11a9c4
Gaganpreet Kaur <kaurgagan951@gmail.com>
2023-09-29 16:16:49 -0400

    Merge branch 'master' of https://github.com/gaganpreetkaur70/DSAlgoPortal.git

--- src/test/java/module1/DataStructure.java
+++ src/test/java/module1/DataStructure.java
@@ -16,99 +16,99 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import dsutilities.LoggerLoad;
 
-public class DataStructure {
-	static WebDriver driver=new ChromeDriver();
+	public class DataStructure {
+		static WebDriver driver=new ChromeDriver();
 	
 	
 @Test (priority=1)
-public void openwebsite() {
-	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"src/test/resources/driver/chromedriver.exe");
-    
-    String url="https://dsportalapp.herokuapp.com/home";
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    driver.get(url);
-    LoggerLoad.info("Website is opened successfully on chrome browser");
-}
-
-
-@Test (priority = 2)
-public void login() throws IOException, InterruptedException {
+	public void openwebsite() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"src/test/resources/driver/chromedriver.exe");
+	    
+	    String url="https://dsportalapp.herokuapp.com/home";
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    driver.get(url);
+	    LoggerLoad.info("Website is opened successfully on chrome browser");
+	}
 	
-	WebElement login = driver.findElement(By.linkText("Sign in"));
-	login.click();
-	LoggerLoad.info("Clicked on the login link");	
 	
-	//Read data from excel file
-	String path = System.getProperty("user.dir")+"/src/test/resources/UserData/User.xlsx";
-	LoggerLoad.info(path);
-	File ExcelFile = new File(path);
-	FileInputStream Fis = new FileInputStream(ExcelFile);
-	XSSFWorkbook workbook = new XSSFWorkbook(Fis);
-	XSSFSheet sheet = workbook.getSheet("Sheet1");
-	Iterator<Row> row = sheet.rowIterator();
-	
-	while(row.hasNext()) {
+	@Test (priority = 2)
+	public void login() throws IOException, InterruptedException {
 		
-		Row currRow = row.next();
+		WebElement login = driver.findElement(By.linkText("Sign in"));
+		login.click();
+		LoggerLoad.info("Clicked on the login link");	
 		
-		if(currRow.getCell(0) != null) {
+		//Read data from excel file
+		String path = System.getProperty("user.dir")+"/src/test/resources/UserData/User.xlsx";
+		LoggerLoad.info(path);
+		File ExcelFile = new File(path);
+		FileInputStream Fis = new FileInputStream(ExcelFile);
+		XSSFWorkbook workbook = new XSSFWorkbook(Fis);
+		XSSFSheet sheet = workbook.getSheet("Sheet1");
+		Iterator<Row> row = sheet.rowIterator();
+		
+		while(row.hasNext()) {
 			
-			driver.findElement(By.id("id_username")).sendKeys(currRow.getCell(0).toString());
-		} 
-		
-		if(currRow.getCell(1) != null) {
-		
-			driver.findElement(By.id("id_password")).sendKeys(currRow.getCell(1).toString());
-		}
-		
-		WebElement loginBtn = driver.findElement(By.xpath("//div//form//input[@type='submit']"));
-		loginBtn.click();
+			Row currRow = row.next();
 			
-			try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		}			
+			if(currRow.getCell(0) != null) {
+				
+				driver.findElement(By.id("id_username")).sendKeys(currRow.getCell(0).toString());
+			} 
+			
+			if(currRow.getCell(1) != null) {
+			
+				driver.findElement(By.id("id_password")).sendKeys(currRow.getCell(1).toString());
+			}
+			
+			WebElement loginBtn = driver.findElement(By.xpath("//div//form//input[@type='submit']"));
+			loginBtn.click();
+				
+				try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			}			
+		
+	}
 	
-}
-
-@Test (priority = 3)
-public void openDataStructure() {
+	@Test (priority = 3)
+	public void openDataStructure() {
+		
+		 driver.findElement(By.xpath("//div[3]/div/div/div/a")).click();
+		 LoggerLoad.info("Data Structure page opened");
+	}
 	
-	 driver.findElement(By.xpath("//div[3]/div/div/div/a")).click();
-	 LoggerLoad.info("Data Structure page opened");
-}
-
-@Test (priority = 4)
-public void openTimeComplexity() {
+	@Test (priority = 4)
+	public void openTimeComplexity() {
+		
+		 driver.findElement(By.xpath("//div[2]/ul/a[@class='list-group-item']")).click();
+		 LoggerLoad.info("Time Complexity page opened");
+	}
 	
-	 driver.findElement(By.xpath("//div[2]/ul/a[@class='list-group-item']")).click();
-	 LoggerLoad.info("Time Complexity page opened");
-}
-
-
-@Test (priority = 5)
-public void openPracticeQuestion() {
 	
-	 driver.findElement(By.xpath("//div[2]/div/div/div/a[@class='list-group-item list-group-item-light text-info']")).click();
-	 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-	 driver.navigate().back();
-	 LoggerLoad.info("Practice Question page opened");
-	 
-}
-
-@Test (priority = 6)
-public void openTryHere() {
+	@Test (priority = 5)
+	public void openPracticeQuestion() {
+		
+		 driver.findElement(By.xpath("//div[2]/div/div/div/a[@class='list-group-item list-group-item-light text-info']")).click();
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		 driver.navigate().back();
+		 LoggerLoad.info("Practice Question page opened");
+		 
+	}
 	
-	 driver.findElement(By.xpath("//div[2]/div/div/a[@class='btn btn-info']")).click();
-	 LoggerLoad.info("Try Here page opened");
-	 
-}
-
-@Test (priority = 7)
-public void closeBrowser() {
-	 driver.close();
-}
+	@Test (priority = 6)
+	public void openTryHere() {
+		
+		 driver.findElement(By.xpath("//div[2]/div/div/a[@class='btn btn-info']")).click();
+		 LoggerLoad.info("Try Here page opened");
+		 
+	}
+	
+	@Test (priority = 7)
+	public void closeBrowser() {
+		 driver.close();
+	}
 }
