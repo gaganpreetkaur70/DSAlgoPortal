6b49e8ab9a659eddb75a9638cd873106c0ce387b
shreyajoshi <shreyajoshi@192.168.0.4>
2023-10-06 19:04:26 -0700

    Crossbrowser

--- src/test/java/module1/NewTestBrowser.java
+++ src/test/java/module1/NewTestBrowser.java
@@ -1,74 +1,59 @@
 package module1;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-public class NewTestBrowser {
-
-    private WebDriver chromedriver;
-    private WebDriver firefoxdriver;
-
+    public class NewTestBrowser {
+    WebDriver driver;
+    
     @Parameters("browser")
-    @BeforeClass
-  
-    public void setup(@Optional("chrome") String browser) { 
+    @BeforeTest
+    public void beforeMytest(String browser) 
+ { 
         if (browser.equalsIgnoreCase("chrome")) {
             //System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-            chromedriver = new ChromeDriver();
+        	driver = new ChromeDriver();
         }
     
-      if (browser.equalsIgnoreCase("firefox")) {
+        else if (browser.equalsIgnoreCase("firefox")) {
             System.setProperty("webdriver.geckodriver", "/Users/shreyajoshi/Downloads/geckodriver");
-            firefoxdriver = new FirefoxDriver();
-        } else {
-            throw new IllegalArgumentException("Invalid browser specified");
+            driver = new FirefoxDriver();
         }
-    }
-
+            else if (browser.equalsIgnoreCase("safari")) {
+                //System.setProperty("webdriver.geckodriver", "/Users/shreyajoshi/Downloads/geckodriver");
+                driver = new SafariDriver(); 
+        } 
+        }
+    
     @Test
-  public void herokuapplogin() {
-    	
-    	 if (chromedriver != null) {
-		String url = "https://dsportalapp.herokuapp.com/home";
-		String expected = "NumpyNinja";
-		chromedriver.get(url);
-		String actual = chromedriver.getTitle();
-		AssertJUnit.assertEquals(actual,expected);
-		WebElement registerLink = chromedriver.findElement(By.linkText("Register"));
-		registerLink.click();
-        }
-    	
+    public void herokuapplogin() {
+    		String url = "https://dsportalapp.herokuapp.com/home";
+    		String expected = "NumpyNinja";
+    		driver.get(url);
+    		String actual = driver.getTitle();
+    		AssertJUnit.assertEquals(actual,expected);
+    		WebElement registerLink = driver.findElement(By.linkText("Register"));
+    		registerLink.click();
+    	    try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    	}
     
-	    if (firefoxdriver != null) {
-	    	String url = "https://dsportalapp.herokuapp.com/home";
-			String expected = "NumpyNinja";
-			firefoxdriver.get(url);
-			String actual = firefoxdriver.getTitle();
-			AssertJUnit.assertEquals(actual,expected);
-			WebElement registerLink = firefoxdriver.findElement(By.linkText("Register"));
-			registerLink.click();
-        
-        }
-	}
-  
-
-    @AfterClass
-    public void tearDown() {
-        if (chromedriver != null) {
-            chromedriver.quit();
-        }
-
-        if (firefoxdriver != null) {
-            firefoxdriver.quit();
-        }
+    
+    @AfterTest
+    public void endTest() {
+  	  driver.quit();
     }
     
     }
+    
